4b250a01c225f8450748721a464fa86cbc4f4006
mnnur <92445384+mnnur@users.noreply.github.com>
2022-01-07 09:53:56 +0700

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,19 @@
-//javascript
+let $ = function(id) {
+    return document.getElementById(id);
+};
+
+let num = 0;
+let multiplier = 1;
+
+setInterval(function () {
+    num += multiplier;
+    if (num > 100){
+        num = 100;
+    }
+    $("loadpercent").textContent = num + "%";
+    if(num === 100){
+        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=1s";
+    }
+    multiplier += num;
+}, 1000);
+
