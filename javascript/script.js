32ab27c2ce85912489acb7a21a809159727bcb08
mnnur <92445384+mnnur@users.noreply.github.com>
2022-02-16 18:10:05 +0700

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -2,20 +2,18 @@ let $ = function(id) {
     return document.getElementById(id);
 };
 
-function genRandRGBA(){
-    const randbg = [];
-    for(let i = 0; i < 4; i++){
-        let x = Math.floor((Math.random() * 255) + 1);
-        randbg.push(x);
+function generateRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
     }
-    let randRGBA = 'rgba(' + randbg[0] + ',' + randbg[1] + ',' + randbg[2] + ',' + randbg[3] + ')';
-    return randRGBA;
+    
+    return color;
 }
 
 function changebg() {
-    let firstcolor = genRandRGBA();
-    let secondcolor = genRandRGBA();
-    document.body.style.backgroundImage = 'linear-gradient(to bottom,' + firstcolor + ',' + secondcolor + ')';
+    document.body.style.backgroundImage = 'linear-gradient(to bottom,' +  generateRandomColor() + ',' +  generateRandomColor() + ')';
 }
 
 let num = 0;
