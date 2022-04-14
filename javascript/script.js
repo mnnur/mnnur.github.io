1a00caf011d42f0a1b0ea168c6d2b3011f2df35c
Muhammad Naufal <nurramadan35@gmail.com>
2022-04-14 13:14:23 +0700

    a

--- javascript/script.js
+++ javascript/script.js
@@ -15,19 +15,3 @@ function generateRandomColor() {
 function changebg() {
     document.body.style.backgroundImage = 'linear-gradient(to bottom,' +  generateRandomColor() + ',' +  generateRandomColor() + ')';
 }
-
-let num = 0;
-let multiplier = 1;
-
-setInterval(function () {
-    num += multiplier;
-    if (num > 100){
-        num = 100;
-    }
-    $("loadpercent").textContent = num + "%";
-    if(num === 100){
-         window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=1s";
-    }
-    multiplier += num;
-}, 1000);
-
