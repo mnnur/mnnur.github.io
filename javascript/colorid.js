06e9086106deba5046b7eb861b06029be35e02c4
Muhammad Naufal <nurramadan35@gmail.com>
2022-04-17 14:31:05 +0700

    update

--- javascript/colorid.js
+++ javascript/colorid.js
@@ -1,8 +1,62 @@
-let $ = function(id) {
+const $ = function (id) {
     return document.getElementById(id);
 };
 
-$('colorPicker').addEventListener("input", function(){
-    let color = $('colorPicker').value;
-    $('view').style.backgroundColor = color; 
+$('colorPicker').addEventListener("input", function () {
+    let color = this.value;
+    let rgb = hexToRgb(color);
+    let red = rgb[0];
+    let green = rgb[1];
+    let blue = rgb[2];
+    $('view').style.backgroundColor = color;
+    $('colorHex').value = color;
+    $('r').value = red;
+    $('g').value = green;
+    $('b').value = blue;
 });
+
+$('colorHex').addEventListener("input", function () {
+    let color = this.value;
+    let rgb = hexToRgb(color);
+    let red = rgb[0];
+    let green = rgb[1];
+    let blue = rgb[2];
+    $('view').style.backgroundColor = color;
+    $('colorPicker').value = color;
+    $('r').value = red;
+    $('g').value = green;
+    $('b').value = blue;
+});
+
+const hexToRgb = hex =>
+    hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
+        , (m, r, g, b) => '#' + r + r + g + g + b + b)
+        .substring(1).match(/.{2}/g)
+        .map(x => parseInt(x, 16))
+
+function decimalToHex(d, padding) {
+    var hex = Number(d).toString(16);
+    padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
+
+    while (hex.length < padding) {
+        hex = "0" + hex;
+    }
+
+    return hex;
+}
+
+function RGBtoHex(r, g, b) {
+    return "#" + decimalToHex(r) + decimalToHex(g) + decimalToHex(b);
+}
+
+document.addEventListener("input", function (e) {
+    if (e.target.id === 'r' || e.target.id === 'g' || e.target.id === 'b') {
+        let r = $('r').value;
+        let g = $('g').value;
+        let b = $('b').value;
+        let color = RGBtoHex(r, g, b);
+        $('view').style.backgroundColor = color;
+        $('colorPicker').value = color;
+        $('colorHex').value = color;
+    }
+})
--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-let $ = function(id) {
+const $ = function (id) {
     return document.getElementById(id);
 };
 
@@ -6,18 +6,18 @@ function generateRandomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 8; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 16)];
     }
-    
+
     return color;
 }
 
 function changebg() {
-    document.body.style.backgroundImage = 'linear-gradient(to bottom,' +  generateRandomColor() + ',' +  generateRandomColor() + ')';
+    document.body.style.backgroundImage = 'linear-gradient(to bottom,' + generateRandomColor() + ',' + generateRandomColor() + ')';
 }
 
 window.addEventListener("load", changebg());
 
-$('btn-1').addEventListener("click", function(){
+$('btn-1').addEventListener("click", function () {
     window.location.href = "/explore/colorid.html";
 });
